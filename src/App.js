f6fb949b20a881f4d219685bfe83786006296d83
mrohitsingh <45506782+mrohitsingh@users.noreply.github.com>
2022-01-31 13:38:59 +0530

    Upload Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import Man from "./img/old-man.jpg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-Box">
+      <div className="App">
+        <img src={Man} alt="man reading a book" />
+      </div>
     </div>
   );
 }
